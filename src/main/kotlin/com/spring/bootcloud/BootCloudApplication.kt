a28f813acefe5f979c017faceba449da91f1f777
talent_mbp2018 <hl010738@126.com>
2019-03-19 16:13:38 +0800

    包结构

--- src/main/kotlin/com/spring/bootcloud/BootCloudApplication.kt
+++ src/main/kotlin/com/spring/bootcloud/BootCloudApplication.kt
@@ -2,10 +2,22 @@ package com.spring.bootcloud
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.Mapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
-class BootCloudApplication
+@RestController
+class BootCloudApplication{
+	@PostMapping(path = ["/test"])
+	fun test(): String{
+		return "ok"
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<BootCloudApplication>(*args)
 }
+
+
