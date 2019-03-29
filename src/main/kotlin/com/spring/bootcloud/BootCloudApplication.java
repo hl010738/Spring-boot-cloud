d1c64a15943d511ef03420f3d01ff834bf4cca66
talent_mbp2018 <hl010738@126.com>
2019-03-29 17:27:25 +0800

    Java代码

---
+++ src/main/kotlin/com/spring/bootcloud/BootCloudApplication.java
@@ -0,0 +1,21 @@
+package com.spring.bootcloud;
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+/*
+    java 代码
+ */
+@SpringBootApplication
+@RestController
+public class BootCloudApplication {
+    public static void main(String[] args) {
+        //SpringApplication.run(BootCloudApplication.class, args);
+
+        SpringApplication springApplication = new SpringApplication(BootCloudApplication.class);
+        springApplication.setBannerMode(Banner.Mode.OFF);
+
+        springApplication.run(args);
+    }
+}
