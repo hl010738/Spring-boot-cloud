9e18fe4917f7342697154b37edba87b5620ef1cb
talent.he <talent.liang.he@accenture.com>
2021-06-01 18:55:22 +0800

    filter

--- src/main/kotlin/com/spring/bootcloud/config/ServletRegistrationConfig.kt
+++ src/main/kotlin/com/spring/bootcloud/config/ServletRegistrationConfig.kt
@@ -1,6 +1,8 @@
 package com.spring.bootcloud.config
 
+import com.spring.bootcloud.filter.Filter2
 import com.spring.bootcloud.servlet.Servlet2
+import org.springframework.boot.web.servlet.FilterRegistrationBean
 import org.springframework.boot.web.servlet.ServletRegistrationBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -16,6 +18,18 @@ class ServletRegistrationConfig {
         bean.addUrlMappings("/servlet2")
 
         bean.servlet = Servlet2()
+
+        return bean
+    }
+
+    @Bean
+    fun getFilterRegistrationBeanConfig(): FilterRegistrationBean<Filter2> {
+
+        var bean = FilterRegistrationBean<Filter2>()
+
+        bean.addUrlPatterns("/servlet1")
+
+        bean.filter = Filter2()
 
         return bean
     }
